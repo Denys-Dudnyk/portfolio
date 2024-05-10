11313a9ed5a8cfdb0aaaf928b8a3100edcb7c71e
Denys-Dudnyk <83369962+Denys-Dudnyk@users.noreply.github.com>
2024-05-10 19:08:11 +0200

    fix bugs

--- components/project.tsx
+++ components/project.tsx
@@ -35,7 +35,7 @@ export default function Project({
 				ref={ref}
 				className='dark:bg-white/10 dark:hover:bg-white/20 dar:text-white rounded-lg cursor-pointer sm:group-even:pl-8 hover:bg-gray-200 transition group mb-3 sm:mb-8 last:mb-0 sm:h-[20rem] relative bg-gray-100 max-w-[60rem] border border-black/5 overflow-hidden sm:pr-8'
 			>
-				<div className=' sm:group-even:ml-[28rem]  h-full flex flex-col pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%]'>
+				<div className=' sm:group-even:ml-[18rem] md:group-even:ml-[25rem] h-full flex flex-col pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%]'>
 					<h3 className='text-2xl font-semibold '>{title}</h3>
 					<p className='dark:text-white/70 mt-2 leading-relaxed text-gray-700 '>
 						{description}
@@ -59,8 +59,7 @@ export default function Project({
 					src={imageUrl}
 					alt='Project I worked on'
 					quality={95}
-					className=' transition group-hover:scale-[1.25] group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-10 group-even:group-hover:rotate-2 group-even:right-[initial] group-even:-left-40 absolute top-8 -right-20 w-[30.25rem] rounded-t-lg shadow-2xl responsive'
-					// hidden sm:block
+					className=' transition group-hover:scale-[1.25] group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-10 group-even:group-hover:rotate-2 group-even:right-[initial] group-even:-left-40 absolute top-8 -right-20 w-[30.25rem] rounded-t-lg shadow-2xl responsive xs:group-hover:scale-100 xs:group-hover:rotate-0 xs:group-hover:translate-y-0 xs:group-hover:-translate-x-0 xs:group-even:left-0 xs:group-even:right-0 xs:group-even:group-hover:rotate-0  xs:group-even:group-hover:translate-y-0 xs:group-even:group-hover:translate-x-0   smMax:group-even:left-0      mdMax:group-hover:scale-100 mdMax:group-hover:rotate-0 mdMax:group-hover:translate-y-0 mdMax:group-hover:-translate-x-0  mdMax:group-even:right-0 mdMax:group-even:group-hover:rotate-0  mdMax:group-even:group-hover:translate-y-0 mdMax:group-even:group-hover:translate-x-0'
 				/>
 			</section>
 		</motion.div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,37 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
-  darkMode: "class",
-};
-export default config;
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic':
+					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			},
+		},
+
+		screens: {
+			xs: { max: '425px' },
+
+			sm: '640px',
+			smMax: { max: '640px' },
+
+			md: '768px',
+			mdMax: { max: '768px' },
+
+			lg: '1024px',
+
+			xl: '1280px',
+
+			'2xl': '1536px',
+		},
+	},
+	plugins: [],
+	darkMode: 'class',
+}
+export default config
